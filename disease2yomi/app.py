50cfe90092767a3ef984a2d654cd1ab5103e5059
Takanori Senoo <takanori_1117@icloud.com>
2023-12-15 21:15:49 +0000

    add yomi estimator for body name

--- disease2yomi/app.py
+++ disease2yomi/app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from datetime import datetime
 import jaconv
-from estimate import estimate_yomi, estimate_icd10, estimate_yomi_from_file, estimate_icd10_from_file
+from estimate import estimate_yomi, estimate_icd10, estimate_yomi_of_body, estimate_yomi_from_file, estimate_icd10_from_file, estimate_yomi_of_body_from_file
 import streamlit as st
 import streamlit_ext as ste
 from stqdm import stqdm
@@ -30,19 +30,27 @@ def set_streamlit():
     st.markdown("病名や症状からそのふりがなとICD-10コードを推定するWebアプリです。")
 
     st.sidebar.write("### サンプルファイルで実行する場合は以下のファイルをダウンロードしてください")
-    sample_csv = pd.read_csv("disease2yomi/data/sample_data_100.csv")
-    sample_csv = sample_csv.to_csv(index=False)
-    ste.sidebar.download_button("sample data", sample_csv, f"disease_name_sample_100.csv")
+    sample_disease_name_csv = pd.read_csv("disease2yomi/data/sample_disease_name_100.csv")
+    sample_disease_name_csv = sample_disease_name_csv.to_csv(index=False)
+    ste.sidebar.download_button("病名のsample data", sample_disease_name_csv, f"sample_disease_name_100.csv")
+    sample_body_name_csv = pd.read_csv("disease2yomi/data/sample_body_name_100.csv")
+    sample_body_name_csv = sample_body_name_csv.to_csv(index=False)
+    ste.sidebar.download_button("部位名のsample data", sample_body_name_csv, f"sample_body_name_100.csv")
 
-    st.sidebar.markdown("### ふりがなとICD-10コードを推定するcsvファイルを選択してください")
+    st.sidebar.markdown("### ふりがなとICD-10コードを推定する病名データの入ったcsvファイルを選択してください")
     # ファイルアップロード
-    uploaded_file = st.sidebar.file_uploader(
-        "Choose a CSV file", accept_multiple_files=False
+    file_uploaded_disease_name = st.sidebar.file_uploader(
+        "Choose a CSV file", accept_multiple_files=False, key="disease_name"
+    )
+    st.sidebar.markdown("### ふりがなを推定する部位名データの入ったcsvファイルを選択してください")
+    # ファイルアップロード
+    file_uploaded_body_name = st.sidebar.file_uploader(
+        "Choose a CSV file", accept_multiple_files=False, key="body_name"
     )
 
     st.sidebar.markdown("奈良先端科学技術大学院大学 ソーシャル・コンピューティング研究室")
 
-    return uploaded_file
+    return file_uploaded_disease_name, file_uploaded_body_name
 
 def convert_to_utf8(content, encoding):
     try:
@@ -143,7 +151,7 @@ def read_uploaded_file_as_utf8(uploaded_file):
 #         return column_df
 
 # def main():
-uploaded_file = set_streamlit()
+file_uploaded_disease_name, file_uploaded_body_name = set_streamlit()
 
 disease_name = st.text_input('病名や症状を入力してください')
 if disease_name:
@@ -166,8 +174,8 @@ if disease_name:
 #     "ICD-10コード",
 # ]
 
-if uploaded_file:
-    df = read_uploaded_file_as_utf8(uploaded_file)
+if file_uploaded_disease_name:
+    df = read_uploaded_file_as_utf8(file_uploaded_disease_name)
     st.write("入力ファイル (先頭5件までを表示)")
     st.dataframe(df.head(5))
     text = st.selectbox(
@@ -208,7 +216,7 @@ if uploaded_file:
         if "completed" not in st.session_state:
             st.session_state["completed"] = True
 
-        file_name_output = uploaded_file.name.replace(".csv", "").replace("disease_name_", "")
+        file_name_output = file_uploaded_disease_name.name.replace(".csv", "")
         timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         csv = output_df.to_csv(index=False)
         # b64 = base64.b64encode(csv.encode("utf-8-sig")).decode()
@@ -218,6 +226,65 @@ if uploaded_file:
         ste.download_button(
             "Click to download data", csv, f"{file_name_output}_yomi_and_icd-10_code_{timestamp}.csv"
         )
+
+body_name = st.text_input('部位名を入力してください')
+if body_name:
+    st.write(f"入力された部位名　：{body_name}")
+    with st.spinner("ふりがなを推定中..."):
+        yomi_of_body = estimate_yomi_of_body(body_name)
+        yomi_of_body = jaconv.kata2hira(yomi_of_body)
+    st.write(f"推定されたふりがな：**{yomi_of_body}**")
+
+if file_uploaded_body_name:
+    df_body = read_uploaded_file_as_utf8(file_uploaded_body_name)
+    st.write("入力ファイル (先頭5件までを表示)")
+    st.dataframe(df_body.head(5))
+    text_body = st.selectbox(
+        "ふりがなを推定する列を選んでください",
+        (df_body.columns),
+        index=None,
+        placeholder="Select...",
+    )
+    st.write("選択した列:", text_body)
+
+    if text_body:
+        with st.spinner("ふりがなを推定中..."):
+            df_output_body = pd.DataFrame()
+            df_output_body[text_body] = df_body[text_body]
+            df_output_body['ふりがな'] = estimate_yomi_of_body_from_file(df=df_output_body, column=text_body)
+            for i in stqdm(range(len(df_output_body))):
+                df_output_body.iat[i, 1] = jaconv.kata2hira(df_output_body.iat[i, 1])
+        st.write("✅ ふりがなの推定が完了しました")
+
+            # # tokenizer, model = generator.download_model()
+            # for i, column in enumerate(target_columns):
+            #     column_df = generator.generate_text(model, tokenizer, df, text, column)
+            #     # display_df = column_df.copy()
+            #     # display_df[text] = display_df[text].iloc[:5].str[:5] + "..."
+
+            #     if i == 0:
+            #         output_df = column_df
+            #         # mytable = st.table(display_df.iloc[:5].T)
+            #     else:
+            #         output_df = pd.merge(output_df, column_df, on=text, how="inner")
+            #         # mytable.add_rows(display_df[[column]].iloc[:5].T)
+
+        st.write("推定結果 (先頭5件までを表示)")
+        st.dataframe(df_output_body.head(5))
+        if "completed" not in st.session_state:
+            st.session_state["completed"] = True
+
+        file_name_output_body = file_uploaded_body_name.name.replace(".csv", "")
+        timestamp_body = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        csv_output_body = df_output_body.to_csv(index=False)
+        # b64 = base64.b64encode(csv.encode("utf-8-sig")).decode()
+        # href = f'<a href="data:application/octet-stream;base64,{b64}" download="{file_name}-riskun-{timestamp}.csv">Download Link</a>'
+        # st.markdown(f"CSVファイルのダウンロード: {href}", unsafe_allow_html=True)
+
+        ste.download_button(
+            "Click to download data", csv_output_body, f"{file_name_output_body}_yomi_{timestamp_body}.csv"
+        )
+
 
 # if __name__ == "__main__":
 # main()
--- disease2yomi/estimate.py
+++ disease2yomi/estimate.py
@@ -285,6 +285,105 @@ def estimate_icd10(disease_name):
     icd10 = generated_titles[0].upper()
     return icd10
 
+def estimate_yomi_of_body(disease_name):
+    # disease2yomi_v1.ipynb
+    # 事前学習済みモデル
+    PRETRAINED_MODEL_NAME = "sonoisa/t5-base-japanese"
+
+    # 転移学習済みモデル
+    MODEL_DIR = f"disease2yomi/trained_model/to_yomi_of_body"
+
+    # https://github.com/neologd/mecab-ipadic-neologd/wiki/Regexp.ja から引用・一部改変
+
+    set_seed(42)
+
+    # GPU利用有無
+    USE_GPU = torch.cuda.is_available()
+
+    # 各種ハイパーパラメータ
+    args_dict = dict(
+        # data_dir="../data",  # データセットのディレクトリ
+        model_name_or_path=PRETRAINED_MODEL_NAME,
+        tokenizer_name_or_path=PRETRAINED_MODEL_NAME,
+
+        learning_rate=3e-4,
+        weight_decay=0.0,
+        adam_epsilon=1e-8,
+        warmup_steps=0,
+        gradient_accumulation_steps=1,
+
+        # max_input_length=512,
+        # max_target_length=64,
+        # train_batch_size=8,
+        # eval_batch_size=8,
+        # num_train_epochs=4,
+
+        n_gpu=1 if USE_GPU else 0,
+        early_stop_callback=False,
+        fp_16=False,
+        opt_level='O1',
+        max_grad_norm=1.0,
+        seed=42,
+    )
+
+    # 学習に用いるハイパーパラメータを設定する
+    args_dict.update({
+        # "max_input_length":  512,  # 入力文の最大トークン数
+        "max_input_length":  128,  # 入力文の最大トークン数
+        "max_target_length": 128,  # 出力文の最大トークン数
+        # "train_batch_size":  8,  # 訓練時のバッチサイズ
+        "train_batch_size":  8,  # 訓練時のバッチサイズ
+        "eval_batch_size":   8,  # テスト時のバッチサイズ
+        "num_train_epochs":  8,  # 訓練するエポック数
+        })
+    args = argparse.Namespace(**args_dict)
+
+    # トークナイザー（SentencePiece）
+    tokenizer = T5Tokenizer.from_pretrained(MODEL_DIR, is_fast=True, local_files_only=True)
+
+    # 学習済みモデル
+    trained_model = T5ForConditionalGeneration.from_pretrained(MODEL_DIR)
+
+    # GPUの利用有無
+    USE_GPU = torch.cuda.is_available()
+    if USE_GPU:
+        trained_model.cuda()
+
+    body = disease_name
+
+    MAX_SOURCE_LENGTH = args.max_input_length   # 入力される記事本文の最大トークン数
+    MAX_TARGET_LENGTH = args.max_target_length  # 生成されるタイトルの最大トークン数
+
+    # 推論モード設定
+    trained_model.eval()
+
+    # 前処理とトークナイズを行う
+    inputs = [get_normalized_text(body)]
+    batch = tokenizer.batch_encode_plus(
+        inputs, max_length=MAX_SOURCE_LENGTH, truncation=True, 
+        padding="longest", return_tensors="pt")
+
+    input_ids = batch['input_ids']
+    input_mask = batch['attention_mask']
+    if USE_GPU:
+        input_ids = input_ids.cuda()
+        input_mask = input_mask.cuda()
+
+    # 生成処理を行う
+    outputs = trained_model.generate(input_ids=input_ids, 
+        attention_mask=input_mask, 
+        max_length=args.max_target_length,
+        temperature=1.0,          # 生成にランダム性を入れる温度パラメータ
+        repetition_penalty=1.5,   # 同じ文の繰り返し（モード崩壊）へのペナルティ
+        )
+
+    generated_titles = [tokenizer.decode(ids, skip_special_tokens=True, 
+                                        clean_up_tokenization_spaces=False) 
+                        for ids in outputs]
+
+    yomi = generated_titles[0]
+    return yomi
+
 def estimate_yomi_from_file(df, column):
     # disease2yomi_v1.ipynb
     # 事前学習済みモデル
@@ -484,6 +583,107 @@ def estimate_icd10_from_file(df, column):
 
         list_icd10[i] = generated_titles[0].upper()
     return list_icd10
+
+def estimate_yomi_of_body_from_file(df, column):
+    # disease2yomi_v1.ipynb
+    # 事前学習済みモデル
+    PRETRAINED_MODEL_NAME = "sonoisa/t5-base-japanese"
+
+    # 転移学習済みモデル
+    MODEL_DIR = f"disease2yomi/trained_model/to_yomi_of_body"
+
+    # https://github.com/neologd/mecab-ipadic-neologd/wiki/Regexp.ja から引用・一部改変
+
+    set_seed(42)
+
+    # GPU利用有無
+    USE_GPU = torch.cuda.is_available()
+
+    # 各種ハイパーパラメータ
+    args_dict = dict(
+        # data_dir="../data",  # データセットのディレクトリ
+        model_name_or_path=PRETRAINED_MODEL_NAME,
+        tokenizer_name_or_path=PRETRAINED_MODEL_NAME,
+
+        learning_rate=3e-4,
+        weight_decay=0.0,
+        adam_epsilon=1e-8,
+        warmup_steps=0,
+        gradient_accumulation_steps=1,
+
+        # max_input_length=512,
+        # max_target_length=64,
+        # train_batch_size=8,
+        # eval_batch_size=8,
+        # num_train_epochs=4,
+
+        n_gpu=1 if USE_GPU else 0,
+        early_stop_callback=False,
+        fp_16=False,
+        opt_level='O1',
+        max_grad_norm=1.0,
+        seed=42,
+    )
+
+    # 学習に用いるハイパーパラメータを設定する
+    args_dict.update({
+        # "max_input_length":  512,  # 入力文の最大トークン数
+        "max_input_length":  128,  # 入力文の最大トークン数
+        "max_target_length": 128,  # 出力文の最大トークン数
+        # "train_batch_size":  8,  # 訓練時のバッチサイズ
+        "train_batch_size":  8,  # 訓練時のバッチサイズ
+        "eval_batch_size":   8,  # テスト時のバッチサイズ
+        "num_train_epochs":  8,  # 訓練するエポック数
+        })
+    args = argparse.Namespace(**args_dict)
+
+    # トークナイザー（SentencePiece）
+    tokenizer = T5Tokenizer.from_pretrained(MODEL_DIR, is_fast=True, local_files_only=True)
+
+    # 学習済みモデル
+    trained_model = T5ForConditionalGeneration.from_pretrained(MODEL_DIR)
+
+    # GPUの利用有無
+    USE_GPU = torch.cuda.is_available()
+    if USE_GPU:
+        trained_model.cuda()
+
+    MAX_SOURCE_LENGTH = args.max_input_length   # 入力される記事本文の最大トークン数
+    MAX_TARGET_LENGTH = args.max_target_length  # 生成されるタイトルの最大トークン数
+
+    # 推論モード設定
+    trained_model.eval()
+
+    list_yomi = [""] * len(df[column])
+    for i in stqdm(range(len(df[column]))):
+        body = df[column][i]
+
+        # 前処理とトークナイズを行う
+        inputs = [get_normalized_text(body)]
+        batch = tokenizer.batch_encode_plus(
+            inputs, max_length=MAX_SOURCE_LENGTH, truncation=True, 
+            padding="longest", return_tensors="pt")
+
+        input_ids = batch['input_ids']
+        input_mask = batch['attention_mask']
+        if USE_GPU:
+            input_ids = input_ids.cuda()
+            input_mask = input_mask.cuda()
+
+        # 生成処理を行う
+        outputs = trained_model.generate(input_ids=input_ids, 
+            attention_mask=input_mask, 
+            max_length=args.max_target_length,
+            temperature=1.0,          # 生成にランダム性を入れる温度パラメータ
+            repetition_penalty=1.5,   # 同じ文の繰り返し（モード崩壊）へのペナルティ
+            )
+
+        generated_titles = [tokenizer.decode(ids, skip_special_tokens=True, 
+                                            clean_up_tokenization_spaces=False) 
+                            for ids in outputs]
+
+        list_yomi[i] = generated_titles[0]
+    return list_yomi
 
 
 # print(estimate_yomi("帯状疱疹"))
